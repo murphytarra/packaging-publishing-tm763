86f08d4d92f1607cfac0337daf4cb8be35d17138
murphytarra <68056855+murphytarra@users.noreply.github.com>
2025-03-11 11:39:38 +0000

    added tests

---
+++ tests/test_CancerModel.py
@@ -0,0 +1,19 @@
+import unittest
+from scripts.cancer_model import CancerModel  # Assuming CancerModel is the class to be tested
+
+class TestCancerModel(unittest.TestCase):
+
+    def setUp(self):
+        self.model = CancerModel()
+
+    def test_initial_state(self):
+        pass
+
+    def test_predict(self):
+        pass
+
+    def test_update_state(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
